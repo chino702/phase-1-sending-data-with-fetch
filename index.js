c7218874f7c0d8b49ff726999000862038c11023
chino702 <chinox3702@gmail.com>
2023-04-19 14:48:47 -0700

    sending with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        const newElement = document.createElement('div');
+        newElement.innerHTML = id;
+        document.body.appendChild(newElement);
+      })
+      .catch(error => {
+        const newElement = document.createElement('div');
+        newElement.innerHTML = error.message;
+        document.body.appendChild(newElement);
+      });
+  }
+  
